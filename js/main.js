1b36593ae9cd047c3b30c4b192916c62dd0e628c
saadmalik8806 <82382491+saadmalik8806@users.noreply.github.com>
2025-05-19 14:02:45 +0530

    Update main.js
    working on load time

--- js/main.js
+++ js/main.js
@@ -127,28 +127,23 @@
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
    ------------------------------------------------------ */  
-   var toggleButton = $('.menu-toggle'),
-       nav = $('.main-navigation');
+ var toggleButton = $('.menu-toggle'),
+    nav = $('.main-navigation');
 
-   // toggle button
-   toggleButton.on('click', function(e) {
+// toggle button
+toggleButton.on('click', function(e) {
+  e.preventDefault();
+  toggleButton.toggleClass('is-clicked');
+  nav.slideToggle();
+});
 
-		e.preventDefault();
-		toggleButton.toggleClass('is-clicked');
-		nav.slideToggle();
-
-	});
-
-   // nav items
-  	nav.find('li a').on("click", function() {   
-
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
-   	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
-  	});
-
+// nav items
+nav.find('li a').on("click", function() {
+  // update the toggle button 		
+  toggleButton.toggleClass('is-clicked'); 
+  // fade out the navigation panel
+  nav.fadeOut();   		
+});
 
    /*---------------------------------------------------- */
   	/* Highlight the current section in the navigation bar
